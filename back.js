10c714823a6314c74199aede3e3b4ce7c5396be6
GER-ITL <danteryzvelt@inbox.ru>
2023-08-28 07:03:49 +0300

    loremjs

--- back.js
+++ back.js
@@ -2,3 +2,4 @@ document.body.innerHTML = '<h1>Hello JS</h1>'
 document.body.innerHTML = '<h1>Hello JS</h1>'
 document.body.innerHTML = '<h1>Hello JS</h1>'
 document.body.ATTRIBUTE_NODE
+document.body.innerHTML = '<h2>lorem</h2>'
